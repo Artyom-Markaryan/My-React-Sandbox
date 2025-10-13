16722a3cfbd5671245e09a99771b55062adf34f3
Artyom Markaryan <artyommarkaryan29@gmail.com>
2025-10-13 14:18:27 -0400

    Créé le projet React + Vite avec Tailwind CSS

---
+++ my-react-sandbox/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css"
+
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-sky-100">
+      <h1 className="text-4xl font-bold text-sky-600">
+        Hello React + Vite + Tailwind CSS 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
