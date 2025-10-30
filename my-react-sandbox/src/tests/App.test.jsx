8cb3039184bd0a2780e7fb22031bc97fb8524fd9
Artyom Markaryan <artyommarkaryan29@gmail.com>
2025-10-30 14:10:46 -0400

    Ajouté les librairies react-router, react-i18next et vitest

---
+++ my-react-sandbox/src/tests/App.test.jsx
@@ -0,0 +1,11 @@
+﻿import {describe, test, expect} from "vitest";
+import {render, screen} from "@testing-library/react";
+import App from "../App";
+
+describe("my-react-sandbox App tests", () => {
+  test("Hello World element should be in the document", () => {
+    render(<App />)
+    const helloWorldElement = screen.getByText("Hello React + Vite + Tailwind CSS 🚀")
+    expect(helloWorldElement).toBeInTheDocument()
+  })
+})
